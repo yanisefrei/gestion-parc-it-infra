6ecb75d12e86acf74aa9eb511a6d808c877b83a1
yanisefrei <150355512+yanisefrei@users.noreply.github.com>
2024-03-27 17:36:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,42 @@ terraform {
   }
   required_version = ">= 0.14.9"
 }
+
 provider "azurerm" {
   features {}
 }
+
+resource "random_integer" "unique_suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-example-${random_integer.unique_suffix.result}"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "asp-example-${random_integer.unique_suffix.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "webapp-example-${random_integer.unique_suffix.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "DOCKER|nginx"
+  }
+}
+
