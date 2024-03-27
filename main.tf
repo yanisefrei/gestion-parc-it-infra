1eecf77bd5eb07b7206f8e09bd5bbd6d82113e1d
yanisefrei <150355512+yanisefrei@users.noreply.github.com>
2024-03-27 17:21:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+ 
+Voici les ressources que nous allons créer dans le main.tf :
+azurerm_resource_group
+azurerm_service_plan
+azurerm_linux_web_app
 
